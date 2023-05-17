1d6b46dd9c7a7a31b57d876cc33528048763c4fb
Willianr7 <Rafael.net@hotmail.com.br>
2023-05-17 15:08:25 -0300

    crud angular teste

---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTable } from '@angular/material/table';
+import { PeriodicElement } from 'src/app/models/PeriodicElement';
+import { PeriodicElementService } from 'src/app/services/periodicElement.service';
+import { ElementDialogComponent } from 'src/app/shared/element-dialog/element-dialog.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  providers: [PeriodicElementService]
+})
+export class HomeComponent implements OnInit {
+  @ViewChild(MatTable)
+  table!: MatTable<any>;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
+  dataSource!: PeriodicElement[];
+  
+  constructor(
+    public dialog: MatDialog,
+    public periodicElementService: PeriodicElementService
+    ) {
+    this.periodicElementService.getElements()
+      .subscribe((data: PeriodicElement[]) => {
+        this.dataSource = data;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  openDialog(element: PeriodicElement  | null): void {
+    const dialogRef = this.dialog.open(ElementDialogComponent, {
+      width: '250px',
+      data: element === null ? {
+        position: null,
+        name: '',
+        weight: null,
+        symbol: ''
+      } : {
+        position: element.position,
+        name: element.name,
+        weight: element.weight,
+        symbol: element.symbol
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result !== undefined) {
+        console.log(result);
+        if (this.dataSource.map(p => p.position).includes(result.position)){
+          this.dataSource[result.position - 1] = result;
+          this.table.renderRows();
+        } else {
+          this.periodicElementService.createElements(result)
+            .subscribe((data: PeriodicElement) => {
+              this.dataSource.push(result);
+              this.table.renderRows();
+            });
+        
+        }
+        
+      }
+    });
+  }
+
+  editElement(element: PeriodicElement): void {
+    this.openDialog(element);
+  }
+
+  deleteElement(position: number): void {
+    this.periodicElementServgice.deleteElement()
+    this.dataSource = this.dataSource.filter(p => p.position !== position);
+  }
+}
+
